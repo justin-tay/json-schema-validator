37467e36d0dd69e7aff925519a79752458d96eae
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2023-12-28 19:05:06 +0800

    still bad

--- src/main/java/com/networknt/schema/JsonSchema.java
+++ src/main/java/com/networknt/schema/JsonSchema.java
@@ -254,6 +254,17 @@ public class JsonSchema extends BaseJsonValidator {
         JsonSchema ancestor = this;
         while (ancestor.getId() == null) {
             if (null == ancestor.getParentSchema()) break;
+            if (ancestor.getCurrentUri() != null && ancestor.getParentSchema().getCurrentUri() != null) {
+                if (!Objects.equals(ancestor.getCurrentUri().getScheme(), ancestor.getParentSchema().getCurrentUri().getScheme())) {
+                    break;
+                }
+                if (!Objects.equals(ancestor.getCurrentUri().getHost(), ancestor.getParentSchema().getCurrentUri().getHost())) {
+                    break;
+                }
+                if (!Objects.equals(ancestor.getCurrentUri().getPath(), ancestor.getParentSchema().getCurrentUri().getPath())) {
+                    break;
+                }
+            }
             ancestor = ancestor.getParentSchema();
         }
         return ancestor;
