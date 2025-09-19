fab7b55828ed1724077748b5f18b1398b96a1302
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-09-19 13:50:32 +0800

    Remove undertow and directly map resources

--- src/test/java/com/networknt/schema/AbstractJsonSchemaTestSuite.java
+++ src/test/java/com/networknt/schema/AbstractJsonSchemaTestSuite.java
@@ -19,6 +19,8 @@ package com.networknt.schema;
 import com.networknt.schema.SpecVersion.VersionFlag;
 import com.networknt.schema.regex.JDKRegularExpressionFactory;
 import com.networknt.schema.regex.JoniRegularExpressionFactory;
+import com.networknt.schema.resource.InputStreamSource;
+import com.networknt.schema.resource.SchemaLoader;
 import com.networknt.schema.suite.TestCase;
 import com.networknt.schema.suite.TestSource;
 import com.networknt.schema.suite.TestSpec;
@@ -27,6 +29,8 @@ import org.junit.jupiter.api.AssertionFailureBuilder;
 import org.junit.jupiter.api.DynamicNode;
 import org.opentest4j.AssertionFailedError;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
@@ -44,7 +48,7 @@ import static org.junit.jupiter.api.Assumptions.abort;
 import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
-abstract class AbstractJsonSchemaTestSuite extends HTTPServiceSupport {
+abstract class AbstractJsonSchemaTestSuite {
 
     private static String toForwardSlashPath(Path file) {
         return file.toString().replace('\\', '/');
@@ -178,7 +182,23 @@ abstract class AbstractJsonSchemaTestSuite extends HTTPServiceSupport {
 
     private JsonSchemaFactory buildValidatorFactory(VersionFlag defaultVersion, TestCase testCase) {
         if (testCase.isDisabled()) return null;
-
+        SchemaLoader schemaLoader = new SchemaLoader() {
+            @Override
+            public InputStreamSource getSchema(AbsoluteIri absoluteIri) {
+                String iri = absoluteIri.toString();
+                if (iri.startsWith("http://localhost:1234")) {
+                    return () -> {
+                        String path = iri.substring("http://localhost:1234".length());
+                        File file = new File("src/test/resources/remotes" + path);
+                        if (file.exists()) {
+                            return new FileInputStream("src/test/resources/remotes" + path);
+                        }
+                        return new FileInputStream("src/test/suite/remotes" + path);
+                    };
+                }
+                return null;
+            }
+        };
         VersionFlag specVersion = detectVersion(testCase.getSchema(), testCase.getSpecification(), defaultVersion, false);
         JsonSchemaFactory base = JsonSchemaFactory.getInstance(specVersion);
         return JsonSchemaFactory
@@ -186,6 +206,7 @@ abstract class AbstractJsonSchemaTestSuite extends HTTPServiceSupport {
                 .schemaMappers(schemaMappers -> schemaMappers
                         .mapPrefix("https://", "http://")
                         .mapPrefix("http://json-schema.org", "resource:"))
+                .schemaLoaders(schemaLoaders -> schemaLoaders.add(schemaLoader))               
                 .build();
     }
 
@@ -237,6 +258,9 @@ abstract class AbstractJsonSchemaTestSuite extends HTTPServiceSupport {
         } finally {
             cleanup();
         }
+    }
+
+    protected void cleanup() {
     }
 
     private List<Path> findTestCases(String basePath) {
