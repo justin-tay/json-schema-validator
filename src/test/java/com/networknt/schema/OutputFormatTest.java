512b465a4d8030e557b1987a5d224b6bedeaf1e8
Justin Tay <49700559+justin-tay@users.noreply.github.com>
2025-09-21 18:29:34 +0800

    Remove hard coded instance location for the error message

--- src/test/java/com/networknt/schema/OutputFormatTest.java
+++ src/test/java/com/networknt/schema/OutputFormatTest.java
@@ -42,7 +42,7 @@ class OutputFormatTest {
         Assertions.assertEquals(3, errors.size());
 
         Set<String[]> messages = errors.stream().map(m -> new String[] { m.getEvaluationPath().toString(),
-                m.getSchemaLocation().toString(), m.getInstanceLocation().toString(), m.getMessage() })
+                m.getSchemaLocation().toString(), m.getInstanceLocation().toString(), m.toString() })
                 .collect(Collectors.toSet());
         
         assertThat(messages,
